d0b6b739521f12f6ff339f877ca98199ba4d015e
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2024-02-16 00:30:31 +0100

    Exercici 6

--- presupuestos/src/components/webOptions.jsx
+++ presupuestos/src/components/webOptions.jsx
@@ -1,5 +1,6 @@
 import React, { useState, } from 'react';
 import '../styles/webOptions.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function WebOptions({ updateWebCost }) {
     
@@ -27,6 +28,7 @@ function WebOptions({ updateWebCost }) {
 
             <div className='container-option-pages'>
                 <label>Número de páginas:</label>
+                  <button className='button-info' type="button" data-bs-toggle="modal" data-bs-target="#pagesHelpModal">ℹ️</button>
                   <button className='button-minus' onClick={() => numPagesChange(-1)}>-</button>
                     <input className='input-pages'
                       value={numPages}
@@ -38,6 +40,7 @@ function WebOptions({ updateWebCost }) {
 
             <div className='container-option-languages'>
                 <label>Número de idiomas:</label>
+                  <button className='button-info' type="button" data-bs-toggle="modal" data-bs-target="#languagesHelpModal">ℹ️</button>
                   <button className='button-minus' onClick={() => numLanguagesChange(-1)}>-</button>
                     <input className='input-languages'
                       value={numLanguages}
@@ -51,8 +54,35 @@ function WebOptions({ updateWebCost }) {
                 <h5>Coste total opciones: {webCost} €</h5>
             </div>
             
+            <div className="modal fade" id="pagesHelpModal" tabIndex="-1" aria-labelledby="pagesHelpModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="pagesHelpModalLabel">Número de Páginas</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            Añade el número de páginas que desees para tu proyecto.<br/> El coste por página es de 30€.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="modal fade" id="languagesHelpModal" tabIndex="-1" aria-labelledby="languagesHelpModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="languagesHelpModalLabel">Número de Idiomas</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                        Añade el número de idiomas que necesites para tu proyecto.<br/> El coste por idioma es de 30€.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
-        
     );
 };
 
